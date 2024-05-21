efc263df771adf315a7123f92b330818f962ced9
MOHD.KAIF <121995804+kaif8052@users.noreply.github.com>
2024-05-21 09:41:22 +0000

    container

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553;
+let accountEmail = "kaif@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+accountEmail = "mdjdij@gmail.com";
+accountPassword = "34944";
+accountCity = "banglore"
+
+// accountId = 2; //Can not change because it is constant.
+console.log(accountId);
+
+// Preferrred not to use var because of issue in block scope and functional scope 
+
+
+console.table([accountId, accountEmail,accountPassword,accountCity ]);
