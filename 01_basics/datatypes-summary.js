35a6815f5d530ef50559ffd7cb972f9bb34f4cd7
MOHD.KAIF <121995804+kaif8052@users.noreply.github.com>
2024-05-30 14:31:08 +0000

    commit

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+// PRIMITIVE DATATYPES
+
+// 7 TYPES : String , Number ,Null , Boolean , Undefined , Symbol , Bigint
+
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null 
+
+ let userEmail;
+
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id == anotherId);
+
+ const bigNumber = 132367637n
+
+
+
+
+
+// REFRENCE TYPE (NON PRIMITIVE)
+
+// Array , Objects , Functions
+// Data type of all non primitive is 
+
+const heros = ["shaktiman" , "naagraj" , "doga"]
+
+
+let obj = {
+    name: "kaif",
+    age: 20,
+}
+
+function print(){
+    console.log("kaif");
+}
+
+console.log(typeof obj);
+
+
