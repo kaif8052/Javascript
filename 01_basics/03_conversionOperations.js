0e0213c83fca780926da8ec32f43aacb9c5ec856
MOHD.KAIF <121995804+kaif8052@users.noreply.github.com>
2024-05-30 14:02:30 +0000

    coomit

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,60 @@
+// let score = "33";
+// let score = null
+// let score = undefined
+let score = false;
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc"=>NaN
+// true => 1
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// 1 => true
+// 0 => false
+// "hitesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber );
+
+
+
+// ************** OPERATIONS *************
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+let str1 = "hello"
+let str2 = "kaif"
+
+let str3 = str1 + str2
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+ console.log(+true);  
+//  \donot do this
+
+
+
+// mdn prefix increment postfix  increament
+
+
+
+
