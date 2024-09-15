3e97c097eb471d8156ba66e534aa7f925c78404d
MOHD.KAIF <121995804+kaif8052@users.noreply.github.com>
2024-09-15 18:08:32 +0000

    function

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))  //THIS WILL BE CORRECT AND SHOW NO ERROR
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)  //THIS WILL SHOW ERROR BECAUSE addTwo IS A EXPRESSION 
+const addTwo = function(num){
+    return num + 2
+}
